64f0d1f4558a136f31721a0cb92c144cca275aec
MrDewmon66 <aaydencameron24@gmail.com>
2026-01-02 16:09:12 +1300

    feat: created new react app with ts

--- apps/api/src/server.js
+++ apps/api/src/server.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 
-const PORT = process.env.EXPRESS_PORT || 3000;
+const PORT = process.env.EXPRESS_PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`API running on port: ${PORT}`);
