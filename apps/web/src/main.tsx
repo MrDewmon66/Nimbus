78771e0b9d5a8cf9178dedd045d797977aef55bc
MrDewmon66 <aaydencameron24@gmail.com>
2026-01-04 22:52:16 +1300

    feat: added react routing (for later down the timeline)

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import router from './router.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
